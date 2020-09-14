a26798eb4e57c75bc4681d948900019ff16715cb
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-14 10:16:27 -0700

    index some authCtrl and some sql

--- server/authController.js
+++ server/authController.js
@@ -1,15 +1,27 @@
 const bcrypt = require('bcryptjs')
 
 module.exports = {
+  register: async (req, res) => {
+    //TODO Register new user
+    /*
+      TODO - Get email, password from req.body
+      TODO - Check if user already exists. If they do, reject the request
+      TODO - Salt & hash password
+      TODO - Create the user in the DB
+      TODO - Put user on session
+      TODO - Send results/confirmation
+    */
+
+  },
+
   login: () => {
     //TODO Login existing user
   },
-  register: () => {
-    //TODO Register new user
-  },
+
   logout: () => {
     //TODO Logout user
   },
+
   getUser: () => {
     //TODO Get user from session
   },
--- server/index.js
+++ server/index.js
@@ -1,7 +1,47 @@
 //TODO Basic express setup
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./authController')
+const postCtrl = require('./controller')
+const verifyUser = require('./middlewares/verifyUser')
+
+
+const app = express()
+const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env  
+
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { massAge: 1000 * 60 * 60 * 24 * 365 }, 
+  })
+)
+
 
 //#auth endpoints
 //TODO login, register, logout, getUser
+app.post('/auth/register', verifyUser, authCtrl.register)
+app.post('/auth/login', verifyUser, authCtrl.login)
+app.delete('/auth/logout', verifyUser, authCtrl.logout)
+app.get('/auth/user', verifyUser, authCtrl.getUser) 
+
 
 //#posts endpoints
 //TODO get post put delete posts
+app.get('/api/posts', postCtrl.getPosts)
+app.post('/api/posts', postCtrl.addPost)
+app.put('/api/posts/:post_id', postCtrl.editPost)
+app.delete('/api/posts/:post_id', postCtrl.deletePost)
+
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log("DB is connect cap'n")
+    app.listen(SERVER_PORT, () => console.log(`Crunchatize me on port ${SERVER_PORT}!!!`))
+})
